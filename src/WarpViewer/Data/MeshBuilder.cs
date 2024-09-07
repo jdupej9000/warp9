3f0275ad4957023758e81656cb28c1354cf476ff
Jan Dupej <jdupej9000@gmail.com>
2024-09-07 18:41:33 +0200

    Adding stubs of data structures.

---
+++ src/WarpViewer/Data/MeshBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp9.Data
+{
+    public class MeshBuilder
+    {
+        public MeshBuilder()
+        {
+        }
+
+        public MeshBuilder(Mesh mesh)
+        {
+        }
+
+        public Mesh ToMesh()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
