41c36b838ecf0e753e48cbd1f5b3e425350cda35
Jan Dupej <jdupej9000@gmail.com>
2024-09-07 22:15:56 +0200

    Fields in VolumeBitmap.

--- src/WarpViewer/Data/VolumeBitmap.cs
+++ src/WarpViewer/Data/VolumeBitmap.cs
@@ -8,5 +8,12 @@ namespace Warp9.Data
 {
     public class VolumeBitmap
     {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int Depth { get; private set; }
+        public int RowStrideBytes { get; private set; }
+        public int SliceStrideBytes { get; private set; }
+        public int BytesPerSample { get; private set; }
+        public int SizeBytes => SliceStrideBytes * Depth * BytesPerSample;
     }
 }
