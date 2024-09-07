d73e7fdb222464764076b86c3a707edd155e17f3
Jan Dupej <jdupej9000@gmail.com>
2024-09-07 18:33:49 +0200

    Adding texture stub. Moving viewer files into a separate folder.

---
+++ src/WarpViewer/Viewer/Texture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp9.Viewer
+{
+    public class Texture
+    {
+        // TODO
+    }
+}
