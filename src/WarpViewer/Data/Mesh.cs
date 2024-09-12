3d4ae71c0caf15a10adcc377abd64378d1dd32ef
Jan Dupej <jdupej9000@gmail.com>
2024-09-12 22:45:58 +0200

    Mesh structure.

--- src/WarpViewer/Data/Mesh.cs
+++ src/WarpViewer/Data/Mesh.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpDX.D3DCompiler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,62 @@ using System.Threading.Tasks;
 
 namespace Warp9.Data
 {
+    public enum MeshViewType
+    {
+    }
+
+    public enum MeshSegmentType
+    {
+        Position,
+        Normal,
+        Tex0
+    }
+
+    internal struct MeshSegment
+    {
+        public int NumCoords;
+        public int Offset;
+        public int ItemLength;
+    }
+
     public class Mesh
     {
+        internal Mesh()
+        {
+        }
+
+        Dictionary<MeshSegmentType, MeshSegment> meshSegments = new Dictionary<MeshSegmentType, MeshSegment>();
+        byte[] vertexData, indexData;
+
+        public int VertexCount { get; private init; }
+        public int FaceCount {get; private init; }
+        public bool IsIndexed { get; private init; }
+
+        public bool TryGetRawData(MeshSegmentType kind, int coord, out ReadOnlySpan<byte> data, out int itemSize)
+        {
+            if (meshSegments.TryGetValue(kind, out MeshSegment seg))
+            {
+                if (coord == AllCoords)
+                {
+                    data = new ReadOnlySpan<byte>(vertexData,
+                        seg.Offset,
+                        VertexCount * seg.ItemLength * seg.NumCoords);
+                    itemSize = seg.ItemLength;
+                }
+                else if (coord >= 0 && coord < seg.NumCoords)
+                {
+                    data = new ReadOnlySpan<byte>(vertexData,
+                        seg.Offset + coord * VertexCount * seg.ItemLength,
+                        VertexCount * seg.ItemLength);
+                    itemSize = seg.ItemLength;
+                }
+            }
+
+            data = new ReadOnlySpan<byte>();
+            itemSize = 0;
+            return false;
+        }
+
+        public const int AllCoords = -1;
     }
 }
