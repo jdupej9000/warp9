5749f8a05e481bfcd5f7a25ab6904281f1b53c08
Jan Dupej <jdupej9000@gmail.com>
2024-09-15 22:35:57 +0200

    Stubs.

---
+++ src/WarpViewer/Data/MeshTransform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp9.Data
+{
+    public static class MeshTransform
+    {
+        public static Mesh UnshareVertices(Mesh m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Mesh ShareVertices(Mesh m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
