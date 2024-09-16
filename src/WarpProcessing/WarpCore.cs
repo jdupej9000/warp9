08dfcd594e5db1a4b38cbfc05cbb318debe13510
Jan Dupej <jdupej9000@gmail.com>
2024-09-16 23:57:56 +0200

    WarpProcessing stub.

---
+++ src/WarpProcessing/WarpCore.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Warp9
+{
+    public static class WarpCore
+    {
+        public const int WCINFO_VERSION = 0;
+        public const int WCINFO_MKL_VERSION = 1;
+        public const int WCINFO_MKL_ISA = 2;
+
+
+        [DllImport("WarpCore")]
+        public static extern int wcore_get_info(int index, StringBuilder buffer, int bufferSize);
+    }
+}
