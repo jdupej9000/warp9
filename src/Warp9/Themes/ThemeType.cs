98f7a8fea891086dd3bf23f1f11d05e15a37e468
Jan Dupej <jdupej9000@gmail.com>
2024-09-20 23:02:24 +0200

    Changing WPF-DX interop package. Adding WPFDarkTheme code for a nice look.

---
+++ src/Warp9/Themes/ThemeType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// The themes are copied with namespaces modified from
+// https://github.com/AngryCarrot789/WPFDarkTheme
+namespace Warp9.Themes
+{
+    public enum ThemeType
+    {
+        SoftDark,
+        RedBlackTheme,
+        DeepDark,
+        GreyTheme,
+        DarkGreyTheme,
+        LightTheme,
+    }
+
+    public static class ThemeTypeExtension
+    {
+        public static string GetName(this ThemeType type)
+        {
+            switch (type)
+            {
+                case ThemeType.SoftDark: return "SoftDark";
+                case ThemeType.RedBlackTheme: return "RedBlackTheme";
+                case ThemeType.DeepDark: return "DeepDark";
+                case ThemeType.GreyTheme: return "GreyTheme";
+                case ThemeType.DarkGreyTheme: return "DarkGreyTheme";
+                case ThemeType.LightTheme: return "LightTheme";
+                default: throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+        }
+    }
+}
