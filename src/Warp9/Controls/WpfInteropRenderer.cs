6516a846dae260c6cfceb27e3de1249523a644eb
jdupej9000 <jdupej9000@gmail.com>
2024-09-20 17:01:14 +0200

    DX11 rendering toy that interops with WPF.

---
+++ src/Warp9/Controls/WpfInteropRenderer.cs
@@ -0,0 +1,150 @@
+﻿using SharpDX;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using Warp9.Viewer;
+
+namespace Warp9.Controls
+{
+    public class WpfInteropRenderer : RendererBase
+    {
+        private WpfInteropRenderer(SharpDX.DXGI.AdapterDescription desc, Device d)
+        {
+            device = d;
+            ctx = d.ImmediateContext;
+            deviceDesc = desc;
+            stateCache = new StateCache(d);
+        }
+
+        RenderTargetView? renderTargetView;
+        DepthStencilView? depthStencilView;
+        Texture2D? texFboDepth;
+        Size depthStencilLastSize = Size.Empty;
+
+        public void Present()
+        {
+            if (device is null || ctx is null)
+                throw new InvalidOperationException();
+
+            if (depthStencilView is null || renderTargetView is null)
+                throw new InvalidOperationException();
+
+            ctx.ClearDepthStencilView(depthStencilView,
+                DepthStencilClearFlags.Depth | DepthStencilClearFlags.Stencil, 1.0f, 255);
+            ctx.ClearRenderTargetView(renderTargetView, RenderUtils.ToDxColor(CanvasColor));
+
+            Render();
+
+            ctx.Flush();
+
+        }
+
+        public void EnsureSharedBackBuffer(IntPtr resourcePtr, System.Windows.Size size)
+        {
+            if (device is null || ctx is null) 
+                throw new InvalidOperationException();
+
+            // convert native pointer to DXGI shared resource
+            SharpDX.DXGI.Resource resource = CppObject.FromPointer<SharpDX.DXGI.Resource>(resourcePtr).QueryInterface<SharpDX.DXGI.Resource>();
+
+            // convert shared resource to D3D11 Texture
+            Texture2D sharedBackbuffer = device.OpenSharedResource<Texture2D>(resource.SharedHandle);
+
+            // release reference
+            resource.Dispose();
+
+            // use D3D11 Texture as render target
+            RenderTargetViewDescription desc = new RenderTargetViewDescription();
+            desc.Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm;
+            desc.Dimension = RenderTargetViewDimension.Texture2D;
+            desc.Texture2D.MipSlice = 0;
+
+            renderTargetView = new RenderTargetView(device, sharedBackbuffer, desc);
+
+            // EnsureSharedBackBuffer can be called also on device change, but it is possible that window size
+            // does not change. In that case we may have a stale depth buffer handle. This may not end well.
+            EnsureDepthBuffer(new Size((int)size.Width, (int)size.Height));
+
+            ctx.OutputMerger.SetRenderTargets(depthStencilView, renderTargetView);
+
+            // release reference
+            sharedBackbuffer.Dispose();
+
+            ctx.Rasterizer.SetViewport(0, 0, (int)size.Width, (int)size.Height, 0.0f, 1.0f);
+        }
+
+        public static bool TryCreate(int adapter, out WpfInteropRenderer? ret)
+        {
+            using SharpDX.DXGI.Factory factory = new SharpDX.DXGI.Factory1();
+
+            int adapterIdx = adapter;
+            int numAdapters = factory.GetAdapterCount();
+            if (numAdapters == 0)
+            {
+                ret = null;
+                return false;
+            }
+
+            if (adapterIdx >= numAdapters)
+                adapterIdx = 0;
+
+            SharpDX.DXGI.Adapter a = factory.GetAdapter(adapterIdx);
+
+            try
+            {
+                Device device = new Device(a, DeviceCreationFlags.BgraSupport | DeviceCreationFlags.Debug,
+                    SharpDX.Direct3D.FeatureLevel.Level_11_0);
+
+                ret = new WpfInteropRenderer(a.Description, device);
+                return true;
+            }
+            catch (Exception)
+            {
+                ret = null;
+                return false;
+            }
+        }
+
+        protected void EnsureDepthBuffer(Size size)
+        {
+            if (depthStencilLastSize != size)
+            {
+                if (depthStencilView is not null)
+                    Utilities.Dispose(ref depthStencilView);
+
+                if (texFboDepth is not null)
+                    Utilities.Dispose(ref texFboDepth);
+
+                const SharpDX.DXGI.Format fmtDepth = SharpDX.DXGI.Format.D24_UNorm_S8_UInt;
+
+                texFboDepth = new Texture2D(device, new Texture2DDescription
+                {
+                    ArraySize = 1,
+                    BindFlags = BindFlags.DepthStencil,
+                    Usage = ResourceUsage.Default,
+                    CpuAccessFlags = CpuAccessFlags.None,
+                    Format = fmtDepth,
+                    Height = size.Height,
+                    Width = size.Width,
+                    MipLevels = 1,
+                    OptionFlags = ResourceOptionFlags.None,
+                    SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0)
+                });
+
+                depthStencilView = new DepthStencilView(device, texFboDepth, new DepthStencilViewDescription
+                {
+                    Dimension = DepthStencilViewDimension.Texture2D,
+                    Format = fmtDepth
+                });
+            }
+        }
+    
+
+    }
+}
---
+++ src/Warp9/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using SharpDX.Direct3D11;
+using SharpDX.Mathematics.Interop;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Warp9.Controls;
+using Warp9.Data;
+using Warp9.IO;
+using Warp9.Viewer;
+
+namespace Warp9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        WpfInteropRenderer? renderer;
+        RenderItemMesh? meshRend;
+        TimeSpan lastRender = TimeSpan.Zero;
+        private Random rnd = new Random();
+
+        #region remove
+        public static readonly string AssetsPath = @"../../test/data/";
+
+        public static Stream OpenAsset(string name)
+        {
+            string path = Path.Combine(AssetsPath, name);
+
+            return new FileStream(path, FileMode.Open, FileAccess.Read);
+        }
+        public static Mesh LoadObjAsset(string name, ObjImportMode mode)
+        {
+            using Stream s = OpenAsset(name);
+            if (!ObjImport.TryImport(s, mode, out Mesh m, out string errMsg))
+                throw new InvalidOperationException();
+
+            return m;
+        }
+        #endregion
+
+
+        #region WPF-DX interop
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!WpfInteropRenderer.TryCreate(0, out renderer) || renderer is null)
+                throw new InvalidOperationException();
+
+            renderer.CanvasColor = System.Drawing.Color.Black;
+            renderer.Shaders.AddShader(StockShaders.VsDefault);
+            renderer.Shaders.AddShader(StockShaders.VsDefaultInstanced);
+            renderer.Shaders.AddShader(StockShaders.PsDefault);
+
+            ModelConst mc = new ModelConst();
+            mc.model = Matrix4x4.Identity;
+            renderer.SetConstant(StockShaders.Name_ModelConst, mc);
+
+            Vector3 camera = new Vector3(1.0f, 2.0f, 3.0f);
+            Vector3 at = new Vector3(0, 0, 0);
+            Vector3 up = new Vector3(0, 1, 0);
+            ViewProjConst vpc = new ViewProjConst();
+            vpc.viewProj = Matrix4x4.Transpose(Matrix4x4.CreateLookAtLeftHanded(camera, at, up) *
+               Matrix4x4.CreatePerspectiveFieldOfViewLeftHanded(MathF.PI / 3, 1, 0.01f, 100.0f));
+
+            vpc.camera = new Vector4(camera, 1);
+            renderer.SetConstant(StockShaders.Name_ViewProjConst, vpc);
+
+            CameraLightConst clp = new CameraLightConst();
+            clp.cameraPos = camera;
+            clp.lightPos = camera;
+            renderer.SetConstant(StockShaders.Name_CameraLightConst, clp);
+
+            PshConst pc = new PshConst();
+            pc.color = new Vector4(0, 1, 0, 1);
+            pc.ambStrength = 0.2f;
+            pc.flags = 0;
+            renderer.SetConstant(StockShaders.Name_PshConst, pc);
+
+            meshRend = new RenderItemMesh();
+            meshRend.Mesh = LoadObjAsset("suzanne.obj", ObjImportMode.AllUnshared);
+            meshRend.Style = MeshRenderStyle.ColorFlat | MeshRenderStyle.PhongBlinn;
+            meshRend.ModelMatrix = Matrix4x4.CreateScale(1.5f);
+            meshRend.Color = System.Drawing.Color.Gray;
+            renderer.AddRenderItem(meshRend);
+
+            InteropImage.WindowOwner = (new System.Windows.Interop.WindowInteropHelper(this)).Handle;
+            InteropImage.IsFrontBufferAvailableChanged += InteropImage_IsFrontBufferAvailableChanged;
+            InteropImage.OnRender += OnRender;
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Size size = WpfSizeToPixels(ImageGrid);
+            InteropImage.SetPixelSize((int)size.Width, (int)size.Height);
+        }
+
+        void CompositionTarget_Rendering(object? sender, EventArgs e)
+        {
+            RenderingEventArgs args = (RenderingEventArgs)e;
+            
+            // It's possible for Rendering to call back twice in the same frame 
+            // so only render when we haven't already rendered in this frame.
+            // Also, limit to 60 fps
+            if ((args.RenderingTime - lastRender).TotalSeconds >= 1.0 )
+            {
+                if(meshRend is not null)
+                    meshRend.Color = System.Drawing.Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+                InteropImage.RequestRender();
+                lastRender = args.RenderingTime;
+            }
+        }
+
+        private void InteropImage_IsFrontBufferAvailableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue == false)
+            {
+                // force recreation of lost frontbuffer
+                Size size = WpfSizeToPixels(ImageGrid);
+                InteropImage.SetPixelSize((int)size.Width + 1, (int)size.Height + 1);
+                InteropImage.SetPixelSize((int)size.Width, (int)size.Height);
+                InteropImage.RequestRender();
+            }
+
+        }
+
+        void OnRender(IntPtr resourcePtr, bool isNewSurface)
+        {
+            if(renderer is null)
+                throw new InvalidOperationException();
+
+            if (isNewSurface)
+            {
+                // a new surface has been created (e.g. after a resize)
+                renderer.EnsureSharedBackBuffer(resourcePtr, WpfSizeToPixels(ImageGrid));
+            }
+
+            renderer.Present();
+        }
+        #endregion
+
+
+        private static Size WpfSizeToPixels(FrameworkElement element)
+        {
+            var source = PresentationSource.FromVisual(element);
+            Matrix transformToDevice = source.CompositionTarget.TransformToDevice;
+
+            return (Size)transformToDevice.Transform(new System.Windows.Vector(element.ActualWidth, element.ActualHeight));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            CompositionTarget.Rendering -= CompositionTarget_Rendering;
+            // TODO: renderer dispose
+        }
+    }
+}
