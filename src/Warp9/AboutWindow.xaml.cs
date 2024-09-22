a249a745403a1ea1a7aac9f979e77ebb823374ae
Jan Dupej <jdupej9000@gmail.com>
2024-09-22 21:54:02 +0200

    Attaching to WarpCore.

--- src/Warp9/AboutWindow.xaml.cs
+++ src/Warp9/AboutWindow.xaml.cs
@@ -1,13 +1,53 @@
+using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 
 namespace Warp9
 {
+    public struct WarpCoreDebugItem
+    {
+        public WarpCoreDebugItem(string id, string data)
+        {
+            Id = id;
+            Data = data;
+        }
+
+        public string Id { get; set; }
+        public string Data { get; set; }
+    }
 
     public partial class AboutWindow : Window
     {
         public AboutWindow()
         {
             InitializeComponent();
+        }
+
+        protected ObservableCollection<WarpCoreDebugItem> debugItems = new ObservableCollection<WarpCoreDebugItem>();
+
+        private void PopulateDebugItems()
+        {
+            const int MaxDataLen = 1024;
+            StringBuilder sb = new StringBuilder(MaxDataLen);
+
+            foreach (WarpCoreInfoIndex idx in Enum.GetValues(typeof(WarpCoreInfoIndex)))
+            {
+                int len = WarpCore.wcore_get_info((int)idx, sb, MaxDataLen);
+                debugItems.Add(new WarpCoreDebugItem(
+                    idx.ToString(), sb.ToString()));
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PopulateDebugItems();
+            }
+            catch (DllNotFoundException)
+            {
+                MessageBox.Show("WarpCore is down.");
+            }
         }
     }
 }
--- src/WarpProcessing/WarpCore.cs
+++ src/WarpProcessing/WarpCore.cs
@@ -3,14 +3,16 @@ using System.Text;
 
 namespace Warp9
 {
+    public enum WarpCoreInfoIndex : int
+    {
+        WCINFO_VERSION = 0,
+        WCINFO_MKL_VERSION = 1,
+        WCINFO_MKL_ISA = 2
+    };
+
     public static class WarpCore
     {
-        public const int WCINFO_VERSION = 0;
-        public const int WCINFO_MKL_VERSION = 1;
-        public const int WCINFO_MKL_ISA = 2;
-
-
-        [DllImport("WarpCore")]
+        [DllImport("WarpCore.dll", CharSet = CharSet.Ansi)]
         public static extern int wcore_get_info(int index, StringBuilder buffer, int bufferSize);
     }
 }
