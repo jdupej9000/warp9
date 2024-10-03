8c052a21bc3b89e3d294ef927c7909736c0c3d5b
jdupej9000 <jdupej9000@gmail.com>
2024-10-03 15:38:19 +0200

    Specimen table editing

--- src/Warp9/Navigation/SpecimenTablePage.xaml.cs
+++ src/Warp9/Navigation/SpecimenTablePage.xaml.cs
@@ -50,14 +50,89 @@ namespace Warp9.Navigation
                 throw new InvalidOperationException();
 
             SpecimenTable table = entry.Payload.Table ?? throw new InvalidOperationException();
-            dataMain.ItemsSource = table.GetRows();
+            dataMain.ItemsSource = table.GetRows().ToList();
 
             foreach (var kvp in table.Columns)
             {
-                DataGridTextColumn col = new DataGridTextColumn();
-                col.Header = new SpecimenTableColumnInfo(kvp.Key, kvp.Value.ColumnType.ToString());
-                col.Binding = new Binding("[" + kvp.Key + "]");
-                dataMain.Columns.Add(col);
+
+                switch (kvp.Value.ColumnType)
+                {
+                    case SpecimenTableColumnType.Integer:
+                    case SpecimenTableColumnType.Real:
+                    case SpecimenTableColumnType.String:
+                        {
+                            DataGridTextColumn col = new DataGridTextColumn();
+                            col.Header = new SpecimenTableColumnInfo(kvp.Key, kvp.Value.ColumnType.ToString());
+                            col.Binding = new Binding("[" + kvp.Key + "]");
+                            dataMain.Columns.Add(col);
+                        }
+                        break;
+
+                    case SpecimenTableColumnType.Factor:
+                        {
+                            DataGridComboBoxColumn col = new DataGridComboBoxColumn();
+                            col.Header = new SpecimenTableColumnInfo(kvp.Key, kvp.Value.ColumnType.ToString());
+                            col.SelectedItemBinding = new Binding("[" + kvp.Key + "]");
+                            col.ItemsSource = kvp.Value.Names;
+                            dataMain.Columns.Add(col);
+                        }
+                        break;
+
+                    case SpecimenTableColumnType.Boolean:
+                        {
+                            DataGridCheckBoxColumn col = new DataGridCheckBoxColumn();
+                            col.Header = new SpecimenTableColumnInfo(kvp.Key, kvp.Value.ColumnType.ToString());
+                            col.Binding = new Binding("[" + kvp.Key + "]");
+                            dataMain.Columns.Add(col);
+                        }
+                        break;
+                    case SpecimenTableColumnType.Image:
+                        break;
+                    case SpecimenTableColumnType.Mesh:
+                        break;
+                    case SpecimenTableColumnType.PointCloud:
+                        break;
+                    default:
+                        throw new NotSupportedException();
+                }
+
+               
+            }
+        }
+
+        private void dataMain_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            // This is a workaround for incorrectly drawn combo box backgrounds in the data grid.
+            // see https://github.com/AngryCarrot789/WPFDarkTheme/issues/28
+
+            /*if (isAnyRowEditing())
+            {
+                e.Cancel = true;
+            }*/
+            
+            if (e.Column is DataGridComboBoxColumn cbCol)
+            {
+                Style style = new Style(typeof(ComboBox), 
+                    (Style)TryFindResource("DataGridComboBoxColumnEditingElementStyle"));
+
+                if (style is not null)
+                    cbCol.EditingElementStyle = style;
+            }
+            else if (e.Column is DataGridTextColumn txCol)
+            {
+                Style style = new Style(typeof(TextBox),
+                    (Style)TryFindResource("DataGridTextColumnEditingElementStyle"));
+
+                if (style is not null)
+                    txCol.EditingElementStyle = style;
+            }
+            else if (e.Column is DataGridCheckBoxColumn chCol)
+            {
+                Style style = new Style(typeof(CheckBox),
+                    (Style)TryFindResource("DataGridCheckBoxColumnEditingElementStyle"));
+
+                if (style is not null)
+                    chCol.EditingElementStyle = style;
             }
         }
     }
