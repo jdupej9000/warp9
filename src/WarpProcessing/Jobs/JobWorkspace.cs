94a6539420215a3c3a147c72881f721ba24a17fc
Jan Dupej <jdupej9000@gmail.com>
2024-11-22 22:22:20 +0100

    Implementing JobWorkspace.

--- src/WarpProcessing/Jobs/JobWorkspace.cs
+++ src/WarpProcessing/Jobs/JobWorkspace.cs
@@ -1,27 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Warp9.Jobs
 {
     public class JobWorkspace
     {
+        Dictionary<string, object?> repository = new Dictionary<string, object?>();
+
         public void Set<T>(string key, int index, T value)
         {
+            if (repository.TryGetValue(key, out var val) && val is List<T> list)
+            {
+            }
+            else
+            {
+                list = new List<T>();
+                repository[key] = list;
+            }
             
+            list.EnsureCapacity(index + 1);
+            list[index] = value;
         }
 
         public void Remove(string key)
         {
+            repository.Remove(key);
         }
 
         public bool TryGet<T>(string key, [MaybeNullWhen(false)] out T value)
         {
-            throw new NotImplementedException();
+            if (repository.TryGetValue(key, out var val) && val is T tval)
+            {
+                value = tval;
+                return true;
+            }
+
+            value = default;
+            return false;
         }
 
         public bool TryGet<T>(string key, int index, [MaybeNullWhen(false)] out T value)
         {
-            throw new NotImplementedException();
+            if (repository.TryGetValue(key, out var val) && 
+                val is List<T> list &&
+                list.Count > index)
+            {
+                value = list[index];
+                return true;
+            }
+
+            value = default;
+            return false;
         }
+
     }
 }
