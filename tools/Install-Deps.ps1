d1904d9a074d15bb5ca28b8038564c6a4979b957
jdupej9000 <jdupej9000@gmail.com>
2024-11-29 12:23:35 +0100

    Updating openblas installation script.

--- tools/Install-Deps.ps1
+++ tools/Install-Deps.ps1
@@ -1,7 +1,8 @@
-$ThirdPartyDir = "thirdparty"
+$ThirdPartyDir = "../thirdparty"
 
 $OpenBlasUrl = 'https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.28/OpenBLAS-0.3.28-x64.zip'
 $OpenBlasDir = ([System.IO.Path]::Combine($ThirdPartyDir, 'openblas'))
 New-Item -Path $ThirdPartyDir -Name "openblas" -ItemType "directory"
 Invoke-WebRequest $OpenBlasUrl -OutFile "openblas.zip"
 Expand-Archive 'openblas.zip' -DestinationPath $OpenBlasDir -Force
+Remove-Item 'openblas.zip'
