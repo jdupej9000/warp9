6549955daed5b49dee6f0f869ae122f97eadcf80
jdupej9000 <jdupej9000@gmail.com>
2024-12-05 13:39:45 +0100

    Camera control stub.

---
+++ src/WarpViewer/Viewer/ArcBallCameraControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp9.Viewer
+{
+    public class ArcBallCameraControl : ICameraControl
+    {
+        // https://github.com/mharrys/arcball/blob/master/src/arcball.hpp
+        public event EventHandler<CameraInfo> UpdateView;
+
+        Vector2 viewportSize = Vector2.One;
+
+        public void Grab(Vector2 pt, bool translate)
+        {
+            Vector2 ptn = 2.0f * (pt / viewportSize - Vector2.One);
+
+            throw new NotImplementedException();
+        }
+
+        public void Move(Vector2 pt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Release(Vector2 pt)
+        {
+            viewportSize = pt;
+        }
+
+        public void ResizeViewport(Vector2 size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(Vector3 camera, Vector3 lookat, Vector3 up)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/WarpViewer/Viewer/ICameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warp9.Viewer
+{
+    public struct CameraInfo
+    {
+        public Vector3 Camera, LookAt, Up;
+    }
+
+    public interface ICameraControl
+    {
+        void Grab(Vector2 pt, bool translate);
+        void Move(Vector2 pt);
+        void Release(Vector2 pt);
+        void ResizeViewport(Vector2 size);
+
+        event EventHandler<CameraInfo> UpdateView;
+
+        void Set(Vector3 camera, Vector3 lookat, Vector3 up);
+    }
+}
