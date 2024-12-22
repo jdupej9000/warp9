cb3421144bafbc37f7bd41c1b811ea69758a21b4
Jan Dupej <jdupej9000@gmail.com>
2024-12-22 22:11:32 +0100

    Logging unhandled exceptions.

--- src/Warp9/App.xaml.cs
+++ src/Warp9/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Windows.Themes;
+using System.IO;
 using System.Windows;
 using Warp9.Themes;
 
@@ -10,8 +11,16 @@ namespace Warp9
     public partial class App : Application
     {
         private void Application_Startup(object sender, StartupEventArgs e)
-        {
+        {   
             ThemesController.SetTheme((ThemeType)Options.Instance.ThemeIndex);
+
+            App.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string reportPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "warp9-last-error.txt");
+            File.WriteAllText(reportPath, e.ToString());
         }
     }
 }
