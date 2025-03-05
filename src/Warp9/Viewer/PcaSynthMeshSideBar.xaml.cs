ec81dc444f2702c14218d817642e66e0c986abda
Jan Dupej <jdupej9000@gmail.com>
2025-03-05 23:00:19 +0100

    PCA sidebar.

---
+++ src/Warp9/Viewer/PcaSynthMeshSideBar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warp9.Viewer
+{
+    /// <summary>
+    /// Interaction logic for PcaSynthMeshSideBar.xaml
+    /// </summary>
+    public partial class PcaSynthMeshSideBar : Page
+    {
+        public PcaSynthMeshSideBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Warp9/Viewer/PcaSynthMeshViewerContent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Warp9.Model;
+
+namespace Warp9.Viewer
+{
+    public class PcaSynthMeshViewerContent : ColormapMeshViewerContentBase
+    {
+        public PcaSynthMeshViewerContent(Project proj, long dcaEntityKey, string name) :
+            base(name)
+        {
+            project = proj;
+            entityKey = dcaEntityKey;
+
+            if (!proj.Entries.TryGetValue(entityKey, out ProjectEntry? entry) ||
+                entry is null ||
+                entry.Kind != ProjectEntryKind.MeshPca)
+            {
+                throw new InvalidOperationException();
+            }
+
+            pcaEntry = entry;
+
+            sidebar = new PcaSynthMeshSideBar();
+        }
+
+        Project project;
+        ProjectEntry pcaEntry;
+        long entityKey;
+        PcaSynthMeshSideBar sidebar;
+
+        public override Page? GetSidebar()
+        {
+            return sidebar;
+        }
+    }
+}
