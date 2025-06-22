81cd1f111db8a874da2bfb2b2cb56610ad4c85df
Jan Dupej <jdupej9000@gmail.com>
2025-06-22 22:21:06 +0200

    Update openblas to 0.3.30

--- tools/Install-Deps.ps1
+++ tools/Install-Deps.ps1
@@ -1,6 +1,6 @@
 $ThirdPartyDir = "../thirdparty"
 
-$OpenBlasUrl = "https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.29/OpenBLAS-0.3.29_x64.zip"
+$OpenBlasUrl = "https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.30/OpenBLAS-0.3.30-x64.zip"
 $OpenBlasDir = ([System.IO.Path]::Combine($ThirdPartyDir, 'openblas'))
 New-Item -Path $ThirdPartyDir -Name "openblas" -ItemType "directory"
 Invoke-WebRequest $OpenBlasUrl -OutFile "openblas.zip"
