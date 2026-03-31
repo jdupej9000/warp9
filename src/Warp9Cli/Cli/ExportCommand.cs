e9e3106580560e88920d14030654008cad7f2b38
Jan Dupej <jdupej9000@gmail.com>
2026-03-31 19:19:52 +0200

    Stub of Export command.

---
+++ src/Warp9Cli/Cli/ExportCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warp9Cli.Cli
+{
+    public enum ExportFormat
+    { 
+        Default = 0,
+        Csv = 1
+    }
+
+
+    public class ExportCommandSpec : ICommandSpec
+    {
+        public string? Option => "e";
+
+        public string? LongOption => "export";
+
+        public ICommand Parse(CommandTokens tokens)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExportCommand : ICommand
+    {
+        public ExportCommand(ExportFormat fmt, long item, string? subitem)
+        {
+
+        }
+
+        public void Execute(CommandExecutionContext ctx)
+        {
+            
+        }
+    }
+}
