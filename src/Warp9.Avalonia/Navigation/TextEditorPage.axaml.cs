3856e47287c592b0b50d8ab37528c08c9a148279
Jan Dupej <jdupej9000@gmail.com>
2026-06-12 12:16:57 +0200

    Migration to avalonia.

---
+++ src/Warp9.Avalonia/Navigation/TextEditorPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Warp9.Avalonia;
+
+public partial class TextEditorPage : ContentPage
+{
+    public TextEditorPage()
+    {
+        InitializeComponent();
+    }
+}
