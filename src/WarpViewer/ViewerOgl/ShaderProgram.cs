3c886737a8204a769a659cf6caa0e932e0bb691a
Jan Dupej <jdupej9000@gmail.com>
2026-06-15 23:37:52 +0200

    Shader stub (opengl).

---
+++ src/WarpViewer/ViewerOgl/ShaderProgram.cs
@@ -0,0 +1,30 @@
+﻿using Silk.NET.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warp9.ViewerOgl
+{
+    public class ShaderProgram : IDisposable
+    {
+        private ShaderProgram(GL gl, uint program)
+        {
+            this.gl = gl;
+            this.program = program;
+        }
+
+        GL gl;
+        uint program;
+
+        public void Bind()
+        {
+            gl.UseProgram(program);
+        }
+
+        public void Dispose()
+        {   
+            gl.DeleteProgram(program);
+        }
+
+    }
+}
